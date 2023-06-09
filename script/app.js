ae9971848795f6a206e1f53b7c8a8fe3e391a571
Divij <divijs75@gmail.com>
2023-06-09 19:49:37 +0530

    initialized

---
+++ script/app.js
@@ -0,0 +1,6 @@
+// var key = "8817d8515fb4d71eaa81b8e93ebc0ab0";
+// var cityname="Jabalpur"
+// fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${key}`)
+//     .then(response => response.json())
+//     .then(data=> console.log(data))
+//     .catch(error => console.error(error));
